ff33afcc0b488721425023a69bd603c9a9b2743a
Aziz <azizahmed.dev@gmail.com>
2023-03-17 21:59:44 -0400

    added email and password validation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -49,26 +49,26 @@ const Hero = () => {
 
           <div className="flex justify-around max-w-[500px] my-8 lg:my-2">
             <Link
-              class="relative inline-flex items-center justify-start px-5 py-3 overflow-hidden font-bold rounded-full group"
+              className="relative inline-flex items-center justify-start px-5 py-3 overflow-hidden font-bold rounded-full group"
               onClick={scrollToTrending}
             >
-              <span class="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-buttonSecondary opacity-[3%]"></span>
-              <span class="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-buttonSecondary opacity-100 group-hover:-translate-x-8"></span>
-              <span class="relative w-full text-left text-primary transition-colors duration-200 ease-in-out group-hover:text-btn">
+              <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-buttonSecondary opacity-[3%]"></span>
+              <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-buttonSecondary opacity-100 group-hover:-translate-x-8"></span>
+              <span className="relative w-full text-left text-primary transition-colors duration-200 ease-in-out group-hover:text-btn">
                 Trending Coins
               </span>
-              <span class="absolute inset-0 border-2 border-primary rounded-full"></span>
+              <span className="absolute inset-0 border-2 border-primary rounded-full"></span>
             </Link>
             <Link
-              class="relative inline-flex items-center justify-start px-5 py-3 overflow-hidden font-bold rounded-full group"
+              className="relative inline-flex items-center justify-start px-5 py-3 overflow-hidden font-bold rounded-full group"
               onClick={scrollToAbout}
             >
-              <span class="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-buttonSecondary opacity-[3%]"></span>
-              <span class="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-buttonSecondary opacity-100 group-hover:-translate-x-8"></span>
-              <span class="relative w-full text-left text-primary transition-colors duration-200 ease-in-out group-hover:text-btn">
+              <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-buttonSecondary opacity-[3%]"></span>
+              <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-buttonSecondary opacity-100 group-hover:-translate-x-8"></span>
+              <span className="relative w-full text-left text-primary transition-colors duration-200 ease-in-out group-hover:text-btn">
                 Learn More
               </span>
-              <span class="absolute inset-0 border-2 border-primary rounded-full"></span>
+              <span className="absolute inset-0 border-2 border-primary rounded-full"></span>
             </Link>
           </div>
         </div>
